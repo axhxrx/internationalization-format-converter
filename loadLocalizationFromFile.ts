6c8764451cf12311a593d27dcb26118ff8da35b1
mason <mason@axhxrx.com>
2025-02-02 23:06:30 +0900

    🍒 feat: Implement readLocalizationFromFile() and output conversion to JSON

---
+++ loadLocalizationFromFile.ts
@@ -0,0 +1,126 @@
+import { isLocalization, type Localization } from '@axhxrx/internationalization';
+import { relative } from '@std/path';
+import { stripImports } from './stripImports.ts';
+import { tryImportingCode } from './tryImportingCode.ts';
+
+// deno-lint-ignore no-explicit-any
+type WhoKnows = Record<string, any>;
+
+/**
+ Dynamically imports the TypeScript code from the specified `sourceCode` (which should be the text content of a `Localization` as would be read from a `.ts` file), and throws an error if it is not valid, otherwise returns the i18n definition. Here "valid" means not only valid TypeScript, but valid from the perspective of the rules for `Localization`.
+
+ You can therefore assume that if this method does not throw, the result can be serialized to JSON with `JSON.stringify()`, and it appears to be a valid `Localization` object as defined by `@axhxrx/internationalization` (even though we can't know the `Locales` generic type parameter here, so its a loose check).
+
+ This function has the side effect of writing the code to a temporary file, which is then deleted.
+
+ @param sourceCode The TypeScript code to import — will be written to a temp file an imported.
+
+ @throws {Error} If the TypeScript code is not valid or the format is incorrect.
+
+ FIXME: This should really use a result type instead of throwing, for consistency with `tryImportingCode()` and just general convenience.
+ */
+export const loadLocalizationFileContents = async (
+  sourceCode: string,
+  rootLevelIdentifier?: string,
+) =>
+{
+  const tmpFile = await Deno.makeTempFile({ suffix: '.ts' });
+  await Deno.writeTextFile(tmpFile, sourceCode);
+  const result = await loadLocalizationFromFile(tmpFile, rootLevelIdentifier);
+  await Deno.remove(tmpFile);
+  return result;
+};
+
+/**
+ Loads a `Localization` (a set of translations defined in a specific kind of TypeScript file as defined by `@axhxrx/internationalization`) from a file on disk. This loads the set of translations and a bunch of metadata, which is an intermediate representation of the `Localization` object, and which can then be used to generate some type of output format (e.g. JSON).
+
+ Dynamically imports the TypeScript code from the specified path, and throws an error if it is not valid, otherwise returns the i18n definition. Here "valid" means not only valid TypeScript, but valid from the perspective of the rules for `Localization`.
+
+ You can therefore assume that if this method does not throw, the result can be serialized to JSON with `JSON.stringify()`, and it appears to be a valid `Localization` object as defined by `@axhxrx/internationalization` (even though we can't know the `Locales` generic type parameter here, so its a loose check).
+
+ For this we use Deno's extremely convenient ability to just import the .ts file — because that is just simpler than trying to use the TS AST ourselves. We *could* do that but I don't think it makes sense — even if we moved off Deno, I think Bun can do that or whateverr else we moved to **should** be able to.
+
+ We should avoid using AST processing when not warranted. What we are doing here is simple.
+
+ @param path The path to the TypeScript file to import
+
+ @param rootLevelIdentifier The identifier to use for the root level of the i18n definition. This is important when dealing with multiple `Localization` files and generating a single JSON file, because you need some kind of root-level property name to map to each file. You may specify any arbitrary string, or `{derive: true}` to have the converter automatically derive a name based on the file path relative to the working directory.
+
+ @throws {Error} If the TypeScript code is not valid or the format is incorrect.
+ */
+export const loadLocalizationFromFile = async (
+  path: string,
+  rootLevelIdentifier?: string | { derive: true },
+): Promise<{ module: WhoKnows; originalCode: string; code: string; json: string }> =>
+{
+  const filePath = path;
+  const importResult = await tryImportingCode({ filePath });
+
+  if (!importResult.success)
+  {
+    console.error(`Failed to import file ${filePath}: ${importResult}`);
+    throw new Error(`Failed to import file ${filePath}: ${importResult.error?.message ?? 'unknown error'}`);
+  }
+
+  if (!isLocalization(importResult.module))
+  {
+    console.error(`loadLocalizationFromFile: Invalid i18n structure: ${filePath}`);
+    throw new Error(`loadLocalizationFromFile: Invalid i18n structure`);
+  }
+
+  const originalCode = await Deno.readTextFile(filePath);
+  console.log({ originalCode });
+
+  const code = await stripImports(originalCode);
+  const strippedTmpFile = await Deno.makeTempFile({ suffix: '.ts' });
+  await Deno.writeTextFile(strippedTmpFile, code);
+
+  const reimportResult = await tryImportingCode({ filePath: strippedTmpFile });
+  if (!reimportResult.success)
+  {
+    console.error(
+      `Failed to generate valid code when processing ${path}: the generated code is invalid in file: ${strippedTmpFile}: ${reimportResult}`,
+    );
+    throw new Error(`Failed to reimport file ${strippedTmpFile}: ${reimportResult.error?.message ?? 'unknown error'}`);
+  }
+
+  if (!isLocalization(reimportResult.module))
+  {
+    throw new Error(
+      `Failed to reimport file ${strippedTmpFile}: the imported code is not a valid Localization module`,
+    );
+  }
+  const bareModule: Localization<string> = reimportResult.module as Localization<string>;
+
+  await Deno.remove(strippedTmpFile);
+
+  let rootPropertyName: string | undefined = undefined;
+  if (typeof rootLevelIdentifier === 'object' && rootLevelIdentifier.derive)
+  {
+    const filePathRelativeToCwd = relative(Deno.cwd(), filePath);
+    rootPropertyName = filePathRelativeToCwd;
+  }
+  else if (typeof rootLevelIdentifier === 'string')
+  {
+    rootPropertyName = rootLevelIdentifier;
+  }
+
+  const module = rootPropertyName != null
+    ? { [rootPropertyName]: bareModule }
+    : bareModule;
+
+  const json = JSON.stringify(module, null, 2);
+
+  if (!isLocalization(module))
+  {
+    throw new Error(
+      'loadLocalizationFromFile(): The derived result is not a valid Localization module: invalid i18n structure\n\n',
+      module,
+    );
+  }
+  return { module, originalCode, code, json };
+};
+
+// const inputFile = './test/fixtures/' + 'hoge.nested.i18n.ts';
+// const uno = await loadLocalizationFromFile(inputFile, { derive: true });
+// console.log(Deno.inspect(uno, { depth: Infinity, colors: true }));
---
+++ test/toJSON.test.ts
@@ -0,0 +1,210 @@
+import { assert, assertEquals } from 'jsr:@std/assert';
+
+import { loadLocalizationFromFile } from '../loadLocalizationFromFile.ts';
+
+const pathToFixtures = new URL('./fixtures/', import.meta.url).pathname;
+
+const fooInputFile = pathToFixtures + 'foo.i18n.ts';
+const barInputFile = pathToFixtures + 'bar.i18n.ts';
+const bazInputFile = pathToFixtures + 'baz.i18n.ts';
+
+Deno.test('toJSON converts input file to expected JSON (foo)', async () =>
+{
+  const modulePath = pathToFixtures + 'foo.i18n.ts';
+  console.log(modulePath);
+  const { module, originalCode, code } = await loadLocalizationFromFile(modulePath);
+  console.log({ module, originalCode, code });
+  const j = JSON.stringify(module, null, 2);
+  const expected = Deno.readTextFileSync(pathToFixtures + 'foo.i18n.json');
+  assertEquals(j, expected);
+});
+
+Deno.test('toJSON converts input file to expected JSON (bar)', async () =>
+{
+  const modulePath = pathToFixtures + 'bar.i18n.ts';
+  const x = await loadLocalizationFromFile(modulePath);
+  console.log(x);
+  const j = JSON.stringify(x.module, null, 2);
+  const expected = Deno.readTextFileSync(pathToFixtures + 'bar.i18n.json');
+  assertEquals(j, expected);
+});
+
+Deno.test('toJSON converts input file to expected JSON (baz)', async () =>
+{
+  const modulePath = pathToFixtures + 'baz.i18n.ts';
+  const x = await loadLocalizationFromFile(modulePath);
+  const j = JSON.stringify(x.module, null, 2);
+  const expected = Deno.readTextFileSync(pathToFixtures + 'baz.i18n.json');
+  assertEquals(j, expected);
+});
+
+Deno.test('toJSON with multiple files an top-level identifiers', async () =>
+{
+  const foo = await loadLocalizationFromFile(fooInputFile);
+  const bar = await loadLocalizationFromFile(barInputFile);
+  const baz = await loadLocalizationFromFile(bazInputFile);
+
+  const all = {
+    ...foo.module,
+    ...bar.module,
+    ...baz.module,
+  };
+  const actual = JSON.stringify(all, null, 2);
+
+  const expected = Deno.readTextFileSync(pathToFixtures + 'foo-bar-baz.combined.json');
+
+  assertEquals(actual, expected, 'merged JSON output should match expected');
+});
+
+Deno.test('toJSON with multiple files and no top-level identifiers', async () =>
+{
+  const fooInputFile = pathToFixtures + 'foo.i18n.ts';
+  const barInputFile = pathToFixtures + 'bar.i18n.ts';
+  const bazInputFile = pathToFixtures + 'baz.i18n.ts';
+
+  const foo = await loadLocalizationFromFile(fooInputFile);
+  const bar = await loadLocalizationFromFile(barInputFile);
+  const baz = await loadLocalizationFromFile(bazInputFile);
+
+  const all = {
+    ...foo.module,
+    ...bar.module,
+    ...baz.module,
+  };
+  const actual = JSON.stringify(all, null, 2);
+
+  const expected = Deno.readTextFileSync(pathToFixtures + 'foo-bar-baz.combined.json');
+
+  assertEquals(actual, expected, 'merged JSON output should match expected');
+});
+
+Deno.test('toJSON with multiple files and specified top-level identifiers', async () =>
+{
+  const foo = await loadLocalizationFromFile(fooInputFile, 'hoge');
+  const bar = await loadLocalizationFromFile(barInputFile, 'hige');
+  const baz = await loadLocalizationFromFile(bazInputFile, 'hage');
+
+  const all = {
+    ...foo.module,
+    ...bar.module,
+    ...baz.module,
+  };
+
+  const actual = JSON.stringify(all, null, 2);
+
+  const expected = Deno.readTextFileSync(pathToFixtures + 'foo-bar-baz.combined.custom-identifiers.json');
+
+  assertEquals(actual, expected, 'merged JSON output should match expected');
+});
+
+/**
+ This is maybe the most important test: it simulates a lot of what this library is intended to do: convert several .localization.ts files into a single JSON output.
+ */
+Deno.test('toJSON with multiple files and derived file path-based identifiers', async () =>
+{
+  const foo = await loadLocalizationFromFile(fooInputFile, { derive: true });
+  const bar = await loadLocalizationFromFile(barInputFile, { derive: true });
+  const baz = await loadLocalizationFromFile(bazInputFile, { derive: true });
+
+  const all = {
+    ...foo.module,
+    ...bar.module,
+    ...baz.module,
+  };
+
+  const actual = JSON.stringify(all, null, 2);
+
+  console.log(actual);
+
+  const expected = Deno.readTextFileSync(pathToFixtures + 'foo-bar-baz.combined.derived-identifiers.json');
+
+  assertEquals(actual, expected, 'merged JSON output should match expected');
+});
+
+Deno.test('to JSON with nested references to imported entities', async () =>
+{
+  const inputFile = pathToFixtures + 'hoge.nested.i18n.ts';
+  const result = await loadLocalizationFromFile(inputFile);
+
+  // No type help here; cannot be helped, though — we import unknown data at runtime, so nothing we can do at build time
+  const drink = result.module.hoge.favorite.drink;
+
+  // Thus lets just do:
+  const expected = {
+    hoge: {
+      favorite: {
+        food: {
+          en: 'pizza',
+          ja: 'ピザ',
+        },
+        drink: {
+          alcoholic: {
+            en: 'whiskey',
+            ja: 'ウイスキー',
+          },
+          nonAlcoholic: {
+            en: 'water',
+            ja: '水',
+          },
+        },
+      },
+      nested2: {
+        moreDeeplyNested: {
+          wowser: {
+            en:
+              'The nested property should be completely erased becuase it contains nothing but import references, but nested2 should remain because I, wowser, exist',
+            ja: 'なんか、俺いるぞ',
+          },
+        },
+      },
+    },
+  };
+
+  assertEquals(result.module, expected);
+
+  assertEquals(drink.alcoholic.en, 'whiskey');
+});
+
+Deno.test('to JSON with re-exported references to imported entities', async () =>
+{
+  const inputFile = pathToFixtures + 'hoge.re-exports.i18n.ts';
+  const result = await loadLocalizationFromFile(inputFile);
+
+  // No type help here; cannot be helped, though — we import unknown data at runtime, so nothing we can do at build time
+  const drink = result.module.hoge.favorite.drink;
+
+  // Thus lets just do:
+  const expected = {
+    hoge: {
+      favorite: {
+        food: {
+          en: 'pizza',
+          ja: 'ピザ',
+        },
+        drink: {
+          alcoholic: {
+            en: 'whiskey',
+            ja: 'ウイスキー',
+          },
+          nonAlcoholic: {
+            en: 'water',
+            ja: '水',
+          },
+        },
+      },
+      nested2: {
+        moreDeeplyNested: {
+          wowser: {
+            en:
+              'The nested property should be completely erased becuase it contains nothing but import references, but nested2 should remain because I, wowser, exist',
+            ja: 'なんか、俺いるぞ',
+          },
+        },
+      },
+    },
+  };
+
+  assertEquals(result.module, expected);
+
+  assertEquals(drink.alcoholic.en, 'whiskey');
+});
